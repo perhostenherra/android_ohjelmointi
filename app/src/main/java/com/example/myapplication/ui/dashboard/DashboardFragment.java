e327467611d6bdf3fc47206fb09884cdf5c98fa8
Marjo Jäkärä <marjo9o@hotmail.com>
2022-09-23 11:35:35 +0300

    cjlasdvldjs

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -3,26 +3,47 @@ package com.example.myapplication.ui.dashboard;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class DashboardFragment extends Fragment implements LocationListener {
 
 
+    Geocoder geocoder;
+    List<Address> addresses;
+    String address = addresses.get(0).getAddressLine(0);
+    String city = addresses.get(0).getLocality();
+    String country = addresses.get(0).getCountryName();
+    String postalCode = addresses.get(0).getPostalCode();
     LocationManager locationManager;
+    String currentLocation;
+    Location location;
     Location LastLocation;
+    public static final String TAG = "MyAppMessage";
+    TextView latField;
+    TextView longField;
+    TextView addField;
 
 
     private FragmentDashboardBinding binding;
@@ -34,12 +55,25 @@ public class DashboardFragment extends Fragment {
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        Locale finnish = new Locale("fi", "FI");
+        geocoder = new Geocoder(getContext(), finnish);
+        double latitude = 0;
+        double longitude = 0;
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        latField = root.findViewById(R.id.latitude);
+        longField = root.findViewById(R.id.longitude);
+        addField = root.findViewById(R.id.address);
 
         //final TextView textView = binding.textDashboard;
         //dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
     }
 
     @Override
@@ -55,14 +89,25 @@ public class DashboardFragment extends Fragment {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 20000, 0, (LocationListener) this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 20000, 0, this);
 
+        try {
+            Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(), 1);
+            Address address = addresses.get(0);
+            currentLocation = address.getAddressLine(0);
+        } catch (Exception e) {
+            Log.e(TAG, "Jotain meni pieleen");
+        }
     }
+
 
     @Override
     public void onStop() {
         super.onStop();
-        locationManager.removeUpdates((LocationListener) this);
+        locationManager.removeUpdates(this);
     }
 
     @Override
@@ -70,4 +115,28 @@ public class DashboardFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-}
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+    //makeUseOfNewLocation(location);
+    } public void onStatusChanged(String provider, int status, Bundle extras) {}
+    public void onProviderEnabled(String provider){}
+    public void onProviderDisabled(String provider) {}
+
+}; //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0, locationListener);
