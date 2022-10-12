4ade79902e7d0f80021811b927d47c6df246db09
Marjo Jäkärä <marjo9o@hotmail.com>
2022-10-12 15:48:15 +0300

    Aloitettu rajapintahaku2

--- app/src/main/java/com/example/myapplication/RajapintaHaku.java
+++ app/src/main/java/com/example/myapplication/RajapintaHaku.java
@@ -1,64 +1,66 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Cache;
-import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HurlStack;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class RajapintaHaku extends AppCompatActivity {
 
-
-
+    Request requestQueue;
+    TextView textView;
+    String url = "http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=1000&resultsFrom=0&name=suomen&companyRegistrationFrom=2000-01-01";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rajapinta_haku);
 
-        RequestQueue requestQueue;
 
-// Instantiate the cache
-        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+            // get data via the key
+        String searchTerm = extras.getString("searchTerm");
+        if (searchTerm != null) {
+            url = "http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=1000&resultsFrom=0&name="+searchTerm+"&companyRegistrationFrom=2000-01-01";
+        }
 
-// Set up the network to use HttpURLConnection as the HTTP client.
-        Network network = new BasicNetwork(new HurlStack());
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-// Instantiate the RequestQueue with the cache and network.
-        requestQueue = new RequestQueue(cache, network);
 
-// Start the queue
-        requestQueue.start();
-
-        String url = "http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=1000&resultsFrom=0&name=saimaa&companyRegistrationFrom=2000-01-01";
-
-// Formulate the request and handle the response.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        textView = findViewById(R.id.textView);
+        JsonArrayRequest jor = new JsonArrayRequest (
+                Request.Method.GET, // the HTTP method to use
+                url, // url
+                null, // null indicates, that no parameters will be posted along with request
+                new Response.Listener<JSONArray>() { // listener to receive the Json response
                     @Override
-                    public void onResponse(String response) {
-                        // Do something with the response
-
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONArray responseItems = (JSONArray) response.getJSONArray(Integer.parseInt("results"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
-                new Response.ErrorListener() {
+                new Response.ErrorListener() { // error listener
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // Handle error
                     }
                 });
 
-// Add the request to the RequestQueue.
-        requestQueue.add(stringRequest);
-
-
+            //jor.setRetryPolicy();
+            queue.add(jor);
     }
 }
